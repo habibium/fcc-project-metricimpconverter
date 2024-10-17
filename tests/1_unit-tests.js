7151515cd3b7a49f4526f9dda1994d25c9616cbc
habibium <habib.prodev@gmail.com>
2024-10-18 02:39:29 +0600

    feat: add unit tests for convertHandler.getNum()

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,4 +4,32 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite("Unit Tests", function () {});
+suite("Unit Tests", function () {
+  suite("Test convertHandler.getNum()", () => {
+    test("convertHandler should correctly read a whole number input.", () => {
+      assert.strictEqual(convertHandler.getNum("10gal"), 10);
+    });
+
+    test("convertHandler should correctly read a decimal number input.", () => {
+      assert.strictEqual(convertHandler.getNum("3.14L"), 3.14);
+    });
+
+    test("convertHandler should correctly read a fractional input.", () => {
+      assert.strictEqual(convertHandler.getNum("1/4mi"), 1 / 4);
+    });
+
+    test("convertHandler should correctly read a fractional input with a decimal.", () => {
+      assert.strictEqual(convertHandler.getNum("6/10km"), 6 / 10);
+    });
+
+    test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).", () => {
+      assert.throw(() => convertHandler.getNum("3/2/3lbs"), "invalid number");
+    });
+
+    test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.", () => {
+      assert.strictEqual(convertHandler.getNum("gal"), 1);
+      assert.strictEqual(convertHandler.getNum(""), 1);
+      assert.strictEqual(convertHandler.getNum(), 1);
+    });
+  });
+});
