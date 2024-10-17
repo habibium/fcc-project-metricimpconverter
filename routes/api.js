5dfc82bc11a25df40f19e46ddcf4a0a2c1480f76
habibium <habib.prodev@gmail.com>
2024-10-18 04:43:42 +0600

    feat: add API endpoint for unit conversion

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,26 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
+
+  app.get("/api/convert", (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    });
+  });
 };
